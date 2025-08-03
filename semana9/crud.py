62781ff499740464bf5900b6068d5017a7be0c4a
SebastianGarciaGuerrero <seba.garcia.g@gmail.com>
2025-08-03 10:34:18 -0400

    terminando el crud

--- semana9/crud.py
+++ semana9/crud.py
@@ -4,7 +4,7 @@ def agregar_pacientes(ID, Nombre, Edad, Genero, HistorialMedico, Contacto):
     conn = get_connection()
     if conn:
         cursor = conn.cursor()
-        query = "INSERT INTO clinica ((ID, Nombre, Edad, Genero, HistorialMedico, Contacto) VALUES (%s, %s, %s, %s, %s, %s)"
+        query = "INSERT INTO Pacientes ((ID, Nombre, Edad, Genero, HistorialMedico, Contacto) VALUES (%s, %s, %s, %s, %s, %s)"
         cursor.execute(query, (id, Nombre, Edad, Genero, HistorialMedico, Contacto))
         conn.commit()
         conn.close()
@@ -13,6 +13,32 @@ def obtener_pacientes():
     conn = get_connection()
     if conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM clinica")
+        cursor.execute("SELECT * FROM Pacientes")
         return cursor.fetchall()
+
+def eliminar_pacientes(id):
+    conn = get_connection()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM Pacientes WHERE ID = %s", (id,))
+        conn.commit()
+        conn.close()
+
+def actualizar_pacientes(id, Nombre, Edad, Genero, HistorialMedico, Contacto):
+    conn = get_connection()
+    if conn:
+        cursor = conn.cursor()
+        query = "UPDATE Pacientes SET Nombre=%s, Edad=%s, Genero=%s, HistorialMedico=%s, Contacto=%s WHERE ID=%s"
+        cursor.execute(query, (Nombre, Edad, Genero, HistorialMedico, Contacto, id))
+        conn.commit()
+        conn.close()
+
+def obtener_pacientes():
+    conn = get_connection()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM pacientes")
+        return cursor.fetchall()
+    else:
+        return []
 
--- semana9/db_connection.py
+++ semana9/db_connection.py
@@ -4,13 +4,24 @@ from mysql.connector import Error
 def get_connection():
     try:
         conn = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='mysqlpass',
-            database='clinica',
+            host="localhost",
+            database="clinica",
+            user="root",
+            password="mysqlpass",
             port=3307
         )
-        return conn
+        if conn.is_connected():
+            print("Conexión exitosa a la base de datos 'clinica'")
+            return conn
     except Error as e:
-        print("Error al conectar a la base de datos:", e)
+        print(f"Error al conectar a MySQL: {e}")
         return None
+
+# Función para probar la conexión
+def test_connection():
+    conn = get_connection()
+    if conn:
+        conn.close()
+
+if __name__ == '__main__':
+    test_connection()
